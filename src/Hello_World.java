979c05e88644086f9debad928254b5ba0b4c62ae
wulft <Tom.Wulf@uc.edu>
2026-02-02 11:05:26 -0500

    Hello World

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -2,6 +2,17 @@ public class Hello_World
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World!");
+        int favNum;
+        int doubleFun;
+        String name = "Tom";
+        double salary = 25000.50;
+
+        System.out.println("Hello " + name + "!");  // Output
+        favNum = 5;
+        doubleFun = favNum * 2;
+        System.out.println("Your fav num doubled is " + doubleFun);  // Output
+
+        System.out.println("Your salary is " + salary);  // Output
+
     } // return of main()
 }  // End Class
